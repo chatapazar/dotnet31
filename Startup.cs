089dfb334098954b5920bf4253d310898f2cfb9e
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-24 18:32:34 +0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace Test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddSwaggerGen(c =>
             {
